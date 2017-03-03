f4993e4652ebeb32a052b6a616b4200de0704197
root <root@CINF301.localdomain>
2017-03-03 13:01:55 +0000

    Base code for Controllers

---
+++ app/Controllers/MainController.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Controllers;
+use App\Renderer as Renderer;
+
+
+class MainController
+{
+	public function home()
+	{
+		$title = 'Twitter';
+		
+		$view = new Renderer('views/main/');
+		$view->title = $title;
+		$view->render('home.php');
+	}
+	
+	public function error()
+	{
+		$view = new Renderer('views/main/');
+		$view->render('error.php');
+	}
+
+
+}
